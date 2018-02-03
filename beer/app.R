0acb1e572eda71a442e2903a13e81961ba6f7126
jknappe <jan.knappe@tcd.ie>
2018-02-03 19:21:21 +0000

    dfg

--- beer/app.R
+++ beer/app.R
@@ -8,11 +8,6 @@ library(httr)
 
 options(shiny.sanitize.errors = FALSE)
 
-options(httr_oob_default=TRUE)
-gs_auth(new_user = TRUE)
-gs_ls()
-
-
 # create list of available categories
 categoryList = c("Hoppiness", "Body", "Balance", "Complexity", "Crispiness", "Hipsterness")
 
